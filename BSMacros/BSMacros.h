5b328b85812c0e62149dc0839bf3ce626b0ff586
Bogdan Stasiuk <Bogdan.Stasjuk@gmail.com>
2015-12-19 18:25:47 +0200

    Update BSMacros.h
    Renamed the cap for BSLog from BSLogCap to BSLogx

--- BSMacros/BSMacros.h
+++ BSMacros/BSMacros.h
@@ -18,7 +18,7 @@
 #endif
 
 #define NSLogx(fmt, ...)
-#define BSLogCap(fmt, ...)
+#define BSLogx(fmt, ...)
 #define BSDDLogError(fmt, ...) DDLogError((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
 
 #pragma mark - UIApplication
